45f9de0743e14bd96dbceb790aaf2ada3f2dee36
hoangpham12345 <phamhuyhoang93tn@gmail.com>
2020-09-18 17:28:20 +0700

    add outline of the website

--- testconnect.php
+++ testconnect.php
@@ -1,5 +1,6 @@
 <?php
-   $con = mysqli_connect("127.0.0.1:51110","azure","6#vWHD_$");
+   // $con = mysqli_connect("127.0.0.1:51110","azure","6#vWHD_$");    //for azure
+   $con = mysqli_connect("localhost","root","");    //for localhost
    if (!$con)
    {
     die('Could not connect: ' . mysql_error());
