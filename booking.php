2d5af27ea5f74f2fd4aa0e3680dacfc8b5a93c8c
hoangpham12345 <phamhuyhoang93tn@gmail.com>
2020-09-24 18:15:28 +0700

    Change booking to php, add php constant and testing the query.

--- booking.php
+++ booking.php
@@ -1,9 +1,12 @@
+<?php
+  require_once('database_var.php');
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Railway Management System</title>
-		<link rel="icon" type="image/png" href="images/icon.svg"></link>
+		<link rel="icon" href="images/icon.svg"></link>
 		<link rel="stylesheet" type="text/css" href="index.css"></link>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/p5.js"></script>
--- testconnect.php
+++ testconnect.php
@@ -1,6 +1,6 @@
 <?php
-   // $con = mysqli_connect("127.0.0.1:51110","azure","6#vWHD_$");    //for azure
-   $con = mysqli_connect("localhost","root","");    //for localhost
+  $con = mysqli_connect("127.0.0.1:51110","azure","6#vWHD_$");    //for azure
+   // $con = mysqli_connect("localhost","root","");    //for localhost
    if (!$con)
    {
     die('Could not connect: ' . mysql_error());
