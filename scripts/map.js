c48f540460489061e4a2e71994ab018b6c4d6575
thoai-atb <lybaothoai@gmail.com>
2020-09-25 16:32:21 +0700

    Added time calculating & sql generating tools for the map

--- scripts/map.js
+++ scripts/map.js
@@ -6,9 +6,15 @@ RailMap.background = new Image();
 RailMap.background.src = 'images/railmap.svg';
 RailMap.gridActive = false;
 
+// =========================================================== EVENTS
+
 RailMap.keyDown = function(event) {
   if(event.key == "g"){
       RailMap.gridActive = !RailMap.gridActive;
+  }
+
+  if(event.key == "s"){
+    console.log(RailMap.generateSQL());
   }
 }
 
@@ -16,10 +22,89 @@ RailMap.mouseDown = function(event) {
   let canvas = document.getElementById('map');
   RailMap.mouseX = event.offsetX - parseFloat($(canvas).css('padding-left'));
   RailMap.mouseY = event.offsetY - parseFloat($(canvas).css('padding-top'));
-
-  let date = new Date();
-  let str = date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
 }
+
+// =========================================================== TOOLS
+
+RailMap.generateSQL = function() {
+  schedules = [];
+  schedules[0] = {
+    id: 'TA01',
+    path: ['A01', 'A02', 'A03', 'A04', 'A05', 'A04', 'A06', 'C01', 'C02', 'C01', 'A06', 'A04', 'A03', 'A02', 'A01']
+  }
+  schedules[1] = {
+    id: 'TA05',
+    path: ['A05', 'A04', 'A03', 'A02', 'A01', 'A02', 'B01', 'B02', 'B01', 'A02', 'A03', 'A04', 'A05']
+  }
+  schedules[2] = {
+    id: 'TC02',
+    path: ['C02', 'C01', 'A06', 'A04', 'A03', 'A02', 'A01', 'A02', 'A03', 'A04', 'A05', 'A04', 'A06', 'C01', 'C02']
+  }
+  schedules[3] = {
+    id: 'TB02',
+    path: ['B02', 'B01', 'B03', 'B01', 'A02', 'A03', 'A04', 'A05', 'A04', 'A03', 'A02', 'B01', 'B02']
+  }
+
+  let sql = "";
+  for(let s of schedules){
+    sql += RailMap.generateSQL1(s.id, s.path) + "\n";
+  }
+  return sql;
+}
+
+RailMap.generateSQL1 = function(trainID, stations){
+  let time = new Date();
+  time.setHours(1, 0, 0);
+  let cur = RailMap.stations.get(stations[0]);
+  console.log('start: ' + cur.id + ' time-out: ' + RailMap.formatTime(time));
+
+  let sql = "";
+
+  for(let i = 0; i<stations.length; i++){
+    let next = RailMap.stations.get(stations[i]);
+
+    // Dist
+    let dist = RailMap.distance(cur, next);
+
+    // Time
+    let minutePerUnit = 20;
+    let breakMinute = 5;
+    let timeIN = RailMap.getRoundedDate(5, time.getTime() + (dist * minutePerUnit) * 60 * 1000);
+    let timeOUT = RailMap.getRoundedDate(5, timeIN.getTime() + breakMinute * 60 * 1000);
+
+    sql += RailMap.generateSQL2(trainID, i, stations[i], RailMap.formatTime(timeIN), RailMap.formatTime(timeOUT)) + "\n";
+    cur = next;
+    time = timeOUT;
+  }
+
+  return sql;
+}
+
+RailMap.generateSQL2 = function(trainID, sequenceNumber, stationID, timeIn, timeOut){
+  return "INSERT INTO `schedule` (`train`, `sequence_number`, `station`, `time_in`, `time_out`) VALUES ('"
+  + trainID + "', '" + sequenceNumber + "', '" + stationID + "', '" + timeIn + "', '" + timeOut + "');";
+}
+
+RailMap.getRoundedDate = (minutes, t) => {
+  let ms = 1000 * 60 * minutes; // convert minutes to ms
+  return new Date(Math.round(t / ms) * ms);
+}
+
+RailMap.formatTime = function(date){
+  let h =  date.getHours();
+  h = h<10? "0"+h : h;
+  let m = date.getMinutes();
+  m = m<10? "0"+m : m;
+  return h + ":" + m + ":00";
+}
+
+RailMap.distance = function(stationA, stationB){
+  let dx = stationA.x - stationB.x;
+  let dy = stationA.y - stationB.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+// =========================================================== SETUPS
 
 RailMap.setup = function(){
   let canvas = document.getElementById('map');
@@ -68,6 +153,8 @@ RailMap.addTrain = function(station1, station2, time1, time2){
     t2 : time2
   });
 }
+
+// =========================================================== DISPLAYS
 
 RailMap.display = function(){
   var canvas = document.getElementById('map');
