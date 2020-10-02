4a42fb09f7c5418eaa82b6c666962c14160bab29
thoai-atb <lybaothoai@gmail.com>
2020-10-02 09:51:46 +0700

    Separated css files

--- booking.php
+++ booking.php
@@ -7,7 +7,8 @@
 		<meta charset="utf-8">
 		<title>Railway Management System</title>
 		<link rel="icon" href="images/icon.svg"></link>
-		<link rel="stylesheet" type="text/css" href="index.css"></link>
+		<link rel="stylesheet" type="text/css" href="stylesheet/rms.css"></link>
+		<link rel="stylesheet" type="text/css" href="stylesheet/booking.css"></link>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/p5.js"></script>
 		<script>$(function(){$("#visual-header").load("header.html")});</script>
