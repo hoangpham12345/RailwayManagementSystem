54b39390de506f3112756ae6a052b61bd90c3b4a
Hydro <ssj4gohu@gmail.com>
2020-12-13 20:47:33 +0700

    All information added.

--- about.php
+++ about.php
@@ -12,40 +12,55 @@
 	  	<h1>OUR MISSION</h1>
 	  	<p>To create a railway system easy to use and comfort user from a long day.</p>
 		</div>
+		<h2>CEO</h2>
+		<div class="row">
+			<div class="column">
+				<div class="card">
+					<img src="images/CEO.jpg" alt="" style="width:80%;height:400px;padding-top:20px">
+					<div class="container1">
+		        <p class="cardtitle">Trevor Phillips</p>
+		        <p class="title">CEO</p>
+		        <p>A leading Entrepreneur</p>
+		        <p>@example.com</p>
+		        <p><button class="button">Contact</button></p>
+				</div>
+			</div>
+		</div>
+
 		<h2>OUR TEAM</h2>
 		<div class="row">
 		  <div class="column">
 		    <div class="card">
-		      <img src="images/knight.jpg" alt="Knight" style="width:100%; height:600px">
+		      <img src="images/knight.jpg" alt="Knight" style="width:80%;height:400px;padding-top:20px">
 		      <div class="container1">
-		        <p class="cardtitle">Knight</p>
-		        <p class="title">CEO & Founder</p>
-		        <p>Booking of death.</p>
-		        <p>@example.com</p>
+		        <p class="cardtitle">Lý Bảo Thoại</p>
+		        <p class="title">Leader</p>
+		        <p>Back-end and Booking Tab.</p>
+		        <p>lybaothoai@gmail.com</p>
 		        <p><button class="button">Contact</button></p>
 		      </div>
 		    </div>
 		  </div>
 		  <div class="column">
 		    <div class="card">
-		      <img src="images/knight2.jpg" alt="knight2" style="width:100%; height:600px">
+		      <img src="images/knight2.jpg" alt="knight2" style="width:80%;height:400px;padding-top:20px">
 		      <div class="container1">
-		        <p class="cardtitle">Solaire</p>
+		        <p class="cardtitle">Phạm Huy Hoàng</p>
 		        <p class="title">Director</p>
-		        <p>Controls the flow of page.</p>
-		        <p>@example.com</p>
+		        <p>Back-end and controls the flow of page.</p>
+		        <p>phamhuyhoang@gmail.com</p>
 		        <p><button class="button">Contact</button></p>
 		      </div>
 		    </div>
 		  </div>
 		  <div class="column">
 		    <div class="card">
-		      <img src="images/a.jpg" alt="Sieg" style="width:100%; height:600px">
+		      <img src="images/a.jpg" alt="Sieg" style="width:80%;height:400px;padding-top:20px">
 		      <div class="container1">
-		        <p class="cardtitle">Siegw</p>
+		        <p class="cardtitle">Lê Phan Anh</p>
 		        <p class="title">Designer</p>
-		        <p>Decorate the page.</p>
-		        <p>ssj4@gohu.com</p>
+		        <p>Front-end and update information.</p>
+		        <p>ssj4gohu@gmail.com</p>
 		        <p><button class="button">Contact</button></p>
 		      </div>
 		    </div>
@@ -55,7 +70,7 @@
 		<h2>LOCATION</h2>
 		<div class="column2">
 			<!-- <iframe width="400" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=international%20university&t=&z=13&ie=UTF8&iwloc=&output=embed"  generated code -->
-			<iframe class="card" width="100%" height="600px" src="https://www.google.com/maps/embed?origin=mfe&pb=!1m3!2m1!1sinternational+university!6i16"
+			<iframe class="card" width="92%" height="600px" src="https://www.google.com/maps/embed?origin=mfe&pb=!1m3!2m1!1sinternational+university!6i16"
 				frameborder="0" scrolling="no" marginheight="0" marginwidth="0" text-align="center"></iframe>
 		</div>
 		<?php require_once('footer.php');?>
--- index.php
+++ index.php
@@ -41,23 +41,23 @@
 					<div class="card1" id="Front news">
 						<div class="card1-content">
 							<h3>Health</h3>
-							<a href="https://e.vnexpress.net/photo/news/how-vietnam-produces-its-domestic-covid-19-vaccine-4205025.html"><img src="images/cov.png" alt="" style="width:100%; height:150px"> </a>
-							<p>Stay safe and always wear mask.</p>
+							<a href="https://e.vnexpress.net/photo/news/how-vietnam-produces-its-domestic-covid-19-vaccine-4205025.html"><img src="images/cov.png" alt="" style="width:100%; height:150px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);"> </a>
+							<p>Stay safe and always wear mask in tight public spaces</p>
 						</div>
 					</div>
 					<div class="card1" id="Health">
 						<div class="card1-content">
 								<h3>Life</h3>
-							<a href="https://thanhnien.vn/suc-khoe/dieu-gi-xay-ra-khi-ban-an-moi-ngay-mot-trai-chuoi-1315452.html"><img src="images/health.jpg" alt="" style="width:100%; height:150px"> </a>
+							<a href="https://thanhnien.vn/suc-khoe/dieu-gi-xay-ra-khi-ban-an-moi-ngay-mot-trai-chuoi-1315452.html"><img src="images/health.jpg" alt="" style="width:100%; height:150px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);"> </a>
 							<br>
-							<p>Take care of your body and stay alert. </p>
+							<p>Take care of your body and do regular tests for potential deceases. </p>
 						</div>
 					</div>
 					<div class="card1" id="Travel">
 						<div class="card1-content">
 							<h3>Travel</h3>
-							<a href="https://e.vnexpress.net/news/travel/food/ha-tinh-s-unique-steamed-rice-rolls-with-spring-roll-stuffing-4204927.html"><img src="images/travel.jpg" alt="" style="width:100%; height:150px"> </a>
-							<p>Discover your favorite places.</p>
+							<a href="https://e.vnexpress.net/news/travel/food/ha-tinh-s-unique-steamed-rice-rolls-with-spring-roll-stuffing-4204927.html"><img src="images/travel.jpg" alt="" style="width:100%; height:150px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);"> </a>
+							<p>Discover the interesting cultures your favorite places.</p>
 						</div>
 					</div>
 				</div>
@@ -66,12 +66,27 @@
 		<div class="content">
 			<h1>Announcement <a> <img src="images/Ann.png" alt="" style="width:50px;height:50px"></a></h1>
 			<div class="news-grid"  style="background-color:white; margin-left:5%;margin-right:5%">
-				<div class="blockcontent" >
+				<div class="blockcontent" style="text-align: left;width:100%;">
 					<h2> Update </h2>
-						<p>Currently no issues.</p>
+					<div class="card2">
+						<div class="card2-content" >
+							<h3>Introduction</h3>
+								<p style=""> Trevor Phillips Industry is aiming for a bigger picture by expanding his work and delivery throughout
+									 the country. </p>
+								<p>Through this software, people can book their tickets with knowledge of their paths for the entire run thereby making
+								the trip easy,safe and relaxing.</p>
+						</div>
+				</div>
+				<br>
+				<div class="card2">
+					<div class="card2-content">
+						<h3>Maintainence</h3>
+							<p>Currently the CEO, Mr.Trevor has not ordered us to do anything yet. </p>
+					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 		<br>
 		<?php require_once('footer.php'); ?>
 	</body>
